fb7f44a43006a0452aa8ec06230f8f3140b17047
kaciakk <wlomcj0@gmail.com>
2023-11-07 21:23:31 +0100

    add basic stuff and sing up page

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,15 @@
+
+const Loader = () => {
+  return (
+    <div className="flex-centrer w-full">
+        <img
+         src="/assets/icons/loader.svg"
+         alt="loader"
+         width={24}
+         height={24}
+            />
+        </div>
+  )
+}
+
+export default Loader
